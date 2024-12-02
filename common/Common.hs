c5ce3a2d424297fc30e3c7513de0c5c45ba373bc
Carman Fu <futarimiti@163.com>
2024-12-02 20:11:19 +0000

    add doc

--- common/Common.hs
+++ common/Common.hs
@@ -1,13 +1,14 @@
 module Common
-  ( module Data.Function
+  ( input
+  , counts
+  , count
+  -- * Re-exports
+  , module Data.Function
   , module Control.Monad
   , module Control.Applicative
   , module Control.Lens
   , module Control.Monad.IO.Class
   , module Data.Foldable
-  , input
-  , counts
-  , count
   ) where
 
 import Control.Monad.IO.Class
@@ -22,14 +23,18 @@ import Control.Applicative
 import Control.Lens
 import Data.Foldable
 
+-- | Gets the AoC input file.
 input :: (IsString str, MonadIO m) => m str
 input = liftIO $ do
   path <- getDataFileName "input.txt"
   contents <- readFile path
   pure (fromString contents)
 
+-- | Count occurrences of elements within a @'Foldable'@,
+-- summarising in a @Map@.
 counts :: (Num n, Ord a, Foldable t) => t a -> Map a n
 counts = foldr (\x -> Map.insertWith (+) x 1) mempty
 
-count :: (Num n) => (a -> Bool) -> [a] -> n
-count p = genericLength . mfilter p
+-- | Count occurrences of elements within a @'Foldable'@ and @'MonadPlus'@.
+count :: (Num n, MonadPlus t, Foldable t) => (a -> Bool) -> t a -> n
+count p = fromIntegral . length . mfilter p
