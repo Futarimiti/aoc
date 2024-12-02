6a32af7e6b27dec1fb168ffac0b38569fd58b6bf
Carman Fu <futarimiti@163.com>
2024-12-02 19:57:22 +0000

    2021/2

---
+++ common/MTL.hs
@@ -0,0 +1,26 @@
+-- | MonadXXX classes re-exports
+module MTL
+  ( module Control.Monad.Trans.Class
+  , module Control.Monad.Trans.Maybe
+  , module Control.Monad.IO.Class
+  , module Control.Monad.Fix
+  , module Control.Monad.State
+  , module Control.Monad.Writer
+  , module Control.Monad.Reader
+  , module Control.Monad.RWS
+  , module Control.Monad.Except
+  , module Control.Monad.Catch
+  ) where
+
+
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Maybe
+import Control.Monad.IO.Class
+import Control.Monad.Fix
+import Control.Monad.State
+import Control.Monad.Writer
+import Control.Monad.Reader
+import Control.Monad.RWS
+import Control.Monad.Except
+import Control.Monad.Catch
+
--- src/Y2021/D02.hs
+++ src/Y2021/D02.hs
@@ -1,3 +1,39 @@
-module Y2021.D02 where
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
+module Y2021.D02 (execLine, execLine') where
 
 import Common
+import MTL
+import Pos
+
+-- part1
+execLine :: (MonadState (Pos2 Int) m) => String -> m ()
+execLine (words -> [direction, read -> steps]) = move steps
+  where move = case direction of
+                 "forward" -> forward
+                 "up"      -> upward
+                 "down"    -> downward
+
+-- part2
+
+data Submarine = Submarine
+  { _pos :: Pos2 Int
+  , _aim :: Int
+  } deriving (Show)
+
+makeLenses ''Submarine
+
+instance HasX Submarine Int where
+  x = pos . x
+
+instance HasY Submarine Int where
+  y = pos . y
+
+execLine' :: (MonadState Submarine m) => String -> m ()
+execLine' (words -> [direction, read -> steps]) = case direction of
+  "forward" -> do
+    currAim <- gets (view aim)
+    forward steps
+    downward $ steps * currAim
+  "up"      -> aim -= steps
+  "down"    -> aim += steps
