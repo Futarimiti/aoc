e20d7a62671f022f9d890ab4761dd3eccae537cd
Carman Fu <futarimiti@163.com>
2024-12-02 10:38:22 +0000

    2024/2

--- src/Y2024/D02.hs
+++ src/Y2024/D02.hs
@@ -1,3 +1,17 @@
+{-# LANGUAGE NoOverloadedLists, UnicodeSyntax #-}
 module Y2024.D02 where
 
 import Common
+
+safe :: [Int] -> Bool
+safe = liftA2 (||) inc dec
+  where
+    inc = all (`elem` [1, 2, 3]) . diffs
+    dec = all (`elem` [-1, -2, -3]) . diffs
+
+    diffs :: [Int] -> [Int]
+    diffs = zipWith (-) <*> tail
+
+tolerable :: [Int] -> Bool
+tolerable = liftA2 (||) safe (any safe . remove1)
+  where remove1 xs = [take i xs ++ drop (i + 1) xs | i <- [0 .. length xs - 1]]
