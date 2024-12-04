c356f65de3672ffec6154062fc5b61f36ee1bbcd
Carman Fu <futarimiti@163.com>
2024-12-04 15:39:39 +0000

    2024/4

--- common/Common.hs
+++ common/Common.hs
@@ -11,6 +11,11 @@ module Common
   , module Data.Foldable
   , module Data.Maybe
   , module Witherable
+  , andM
+  , orM
+  , allM
+  , anyM
+  , every
   ) where
 
 import Control.Monad.IO.Class
@@ -19,14 +24,14 @@ import Paths_aoc
 import Data.Function
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Data.List (group, sort)
+import Data.List (group, sort, tails)
 import Control.Monad
 import Control.Applicative
 import Control.Lens
 import Data.Foldable
 import Data.Ord
 import Data.Maybe hiding (catMaybes, mapMaybe)
-import Witherable
+import Witherable hiding (filter)
 
 -- -- | Mode value, in the statistical sense
 -- mode :: Ord a => [a] -> a
@@ -47,3 +52,26 @@ counts = foldr (\x -> Map.insertWith (+) x 1) mempty
 -- | Count occurrences of elements within a @'Foldable'@ and @'MonadPlus'@.
 count :: (Num n, MonadPlus t, Foldable t) => (a -> Bool) -> t a -> n
 count p = fromIntegral . length . mfilter p
+
+anyM :: (Monad m, Foldable f) => (a -> m Bool) -> f a -> m Bool
+anyM f = foldr (orM . f) (pure False)
+
+allM :: (Monad m, Foldable f) => (a -> m Bool) -> f a -> m Bool
+allM f = foldr (andM . f) (pure True)
+
+-- | Monadic version of or
+orM :: Monad m => m Bool -> m Bool -> m Bool
+orM m1 m2 = m1 >>= \x -> if x then return True else m2
+
+-- | Monadic version of and
+andM :: Monad m => m Bool -> m Bool -> m Bool
+andM m1 m2 = m1 >>= \x -> if x then m2 else return False
+
+-- | Every @n@ consecutive elements
+every :: Int -> [a] -> [[a]]
+every n xs = mapMaybe (take' n) (tails xs)
+  where take' :: Int -> [a] -> Maybe [a]
+        take' 0 _      = Just []
+        take' _ []     = Nothing
+        take' n (x:xs) = (x:) <$> take' (n - 1) xs
+
--- src/Y2024/D04.hs
+++ src/Y2024/D04.hs
@@ -1,3 +1,46 @@
-module Y2024.D04 where
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
+module Y2024.D04 (search, searchX) where
 
 import Common
+import MTL
+import Pos
+import Data.List.NonEmpty (NonEmpty (..))
+import Data.List ( transpose, tails, isPrefixOf )
+
+type Board a = [[a]]
+
+horizontals, verticals, diagonals :: Board a -> [[a]]
+horizontals = id
+verticals = transpose
+diagonals = liftA2 (++) diagonal1 (diagonal1 . map reverse)
+  where
+    diagonal1 = liftA2 (++)
+      (transpose . zipWith drop [0..])
+      (transpose . zipWith drop [1..] . rotate180)
+    rotate90 = reverse . transpose
+    rotate180 = rotate90 . rotate90
+
+search :: Eq a => [a] -> Board a -> Int
+search pat board = sum $ map (occurrence pat) allSeqs
+  where
+    allSeqs = horizontals board ++ verticals board ++ diagonals board
+    occurrence pat xs = length $ filter (pat `isPrefixOf`) (tails xs)
+
+-- | get all 3x3 squares
+all3x3 :: Board a -> [[[a]]]
+all3x3 board = do
+  ranks's <- every 3 ranks
+  [xs, ys, zs] <- pure $ map (every 3) ranks's
+  zipWith3 (\x y z -> [x, y, z]) xs ys zs
+  where ranks = board
+
+searchX :: Board Char -> Int
+searchX = count isXMAS . all3x3
+  where
+    isXMAS :: [[Char]] -> Bool
+    isXMAS = \case
+      [ ['M', _, 'M'], [_, 'A', _], ['S', _, 'S'] ] -> True
+      [ ['M', _, 'S'], [_, 'A', _], ['M', _, 'S'] ] -> True
+      [ ['S', _, 'M'], [_, 'A', _], ['S', _, 'M'] ] -> True
+      [ ['S', _, 'S'], [_, 'A', _], ['M', _, 'M'] ] -> True
+      _ -> False
