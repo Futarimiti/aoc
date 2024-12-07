c58c62c036c542ee829c266d368392882fd645d6
Carman Fu <futarimiti@163.com>
2024-12-07 21:08:25 +0000

    2024/7

--- common/MTL.hs
+++ common/MTL.hs
@@ -1,8 +1,7 @@
 -- | MonadXXX classes re-exports
 module MTL
-  ( module Control.Monad.Trans.Class
+  ( module Control.Monad.Trans
   , module Control.Monad.Trans.Maybe
-  , module Control.Monad.IO.Class
   , module Control.Monad.Fix
   , module Control.Monad.State
   , module Control.Monad.Writer
@@ -11,12 +10,13 @@ module MTL
   , module Control.Monad.Except
   , module Control.Monad.Catch
   , module Control.Monad.Logger
+  , module Control.Monad.Accum
+  , module Control.Monad.Trans.Accum
   , exit
   ) where
 
-import Control.Monad.Trans.Class
+import Control.Monad.Trans
 import Control.Monad.Trans.Maybe
-import Control.Monad.IO.Class
 import Control.Monad.Fix
 import Control.Monad.State
 import Control.Monad.Writer
@@ -25,6 +25,8 @@ import Control.Monad.RWS
 import Control.Monad.Except
 import Control.Monad.Catch
 import Control.Monad.Logger
+import Control.Monad.Accum
+import Control.Monad.Trans.Accum hiding (accum, look, looks, add, liftCallCC, liftListen, liftCatch, liftPass)
 
 -- | Early exit
 exit :: MonadError e m => e -> m a
--- src/Y2024/D07.hs
+++ src/Y2024/D07.hs
@@ -1,3 +1,29 @@
+{-# LANGUAGE NoOverloadedLists #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
+
 module Y2024.D07 where
 
 import Common
+import NE
+import L
+import Read
+
+-- I greatly suspect part 2 expands on this
+-- operations = [(+), (*)]
+
+feasible :: (Num a, Eq a)
+         => [a -> a -> a]  -- | possible operations
+         -> NonEmpty a     -- | operands, with fixed order
+         -> a              -- | expected result
+         -> Bool           -- | feasible?
+feasible _ (x:|[]) res = x == res
+feasible ops (x:|y:xs) res = not $ null [() | op <- ops, feasible ops (op x y:|xs) res]
+
+-- | One line
+parse :: forall a. (Read a, Integral a) => ReadP (a, NonEmpty a)
+parse = do
+  res <- uint
+  _ <- string ": "
+  xs <- sepBy1 uint (char ' ')
+  pure (res, fromList xs)  -- safe
+
